0b6cf8b747c8940cef39375ca7fcd880aa65b10e
Takudzwa Mutamburigwa <tmutamburigwa@byupathway.edu>
2025-01-20 12:00:27 +0200

    done

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -169,4 +169,51 @@ public class TakingTurnsQueueTests
             );
         }
     }
+
+    public class TakingTurnsQueue
+    {
+        private Queue<Person> queue = new Queue<Person>();
+
+        public int Length => queue.Count;
+
+        public void AddPerson(string name, int turns)
+        {
+            queue.Enqueue(new Person(name, turns));
+        }
+
+        public Person GetNextPerson()
+        {
+            if (queue.Count == 0)
+            {
+                throw new InvalidOperationException("No one in the queue.");
+            }
+
+            var person = queue.Dequeue();
+
+            if (person.Turns <= 0) // Infinite turns
+            {
+                queue.Enqueue(person);
+            }
+            else if (person.Turns > 1) // Finite turns, decrement and re-add
+            {
+                person.Turns--;
+                queue.Enqueue(person);
+            }
+
+            return person;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; }
+        public int Turns { get; set; }
+
+        public Person(string name, int turns)
+        {
+            Name = name;
+            Turns = turns;
+        }
+    }
+
 }
